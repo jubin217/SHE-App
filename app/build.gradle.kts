6eb156ff096b44129bc9e347a0a5da1091e6fcf9
jubin217 <jubintjoseph7733@gmail.com>
2025-12-18 20:24:11 +0530

    2nd version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,8 @@ dependencies {
 
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
 
+    implementation("com.google.android.material:material:1.11.0")
+
     // Firebase
     implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
     implementation("com.google.firebase:firebase-auth")
